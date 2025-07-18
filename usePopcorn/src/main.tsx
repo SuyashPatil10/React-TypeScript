c5580ee3eed85215014f7580fd9b6813f7a07e4b
Suyash Patil <psuyash@cdac.in>
2025-07-18 20:17:00 +0530

    Added StarRating component

--- usePopcorn/src/main.tsx
+++ usePopcorn/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from './App.tsx'
+import StarRating from "./components/StarRating.tsx";
 
-createRoot(document.getElementById('root')!).render(
+export function Test() {
+  const [movieRating, setMovieRating] = useState<number>(0);
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        color="blue"
+        size={36}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+      defaultRating={3}
+    />
+    <StarRating color="red" size={24} />
+    <Test />
+  </StrictMode>
+);
